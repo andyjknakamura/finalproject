ff3e2e93b2c631c7e9c51ff13625f7ea621d0d31
andyjknakamura <73955056+andyjknakamura@users.noreply.github.com>
2022-12-02 13:11:48 -0500

    set up

---
+++ final.py
@@ -0,0 +1,37 @@
+#AJ Group
+#Jess Yang, Andrew Nakamura
+#SI 206 Final Project
+from xml.sax import parseString
+from bs4 import BeautifulSoup
+import re
+import os
+import csv
+import unittest
+
+#def maps_database:
+#   Jess
+#   Creating the database from the maps API with 
+#   categories such as city name, # of fast food restaurants,
+#   # of supermarkets, # of total restaurants
+
+#def cities_database:
+#   Andrew
+#   Creating the database from urls with
+#   categories such as city name, median household income level, racial demographics of the area,
+#   income change over time(?)
+
+#def restaurant_ratio:
+#   Jess
+#   Calculating ratio of fast food restaurants to total restaurants in the area
+
+#def income_calc:
+#   Andrew
+#   Not sure about this one quite yet tbh
+
+class TestCases(unittest.TestCase):
+    def setUp(self):
+        pass
+
+if __name__ == '__main__':
+    #more code here
+    unittest.main(verbosity=2)
